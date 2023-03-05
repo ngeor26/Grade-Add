fd4bf8acb60820135967bb21288f5bc1592689ae
ngeor26 <ngeor26@germantownacademy.org>
2023-03-05 18:31:58 -0500

    adding files

---
+++ index.js
@@ -0,0 +1,28 @@
+const admin = require('firebase-admin')
+const express = require('express')
+const cors = require('cors')
+const port = process.env.PORT || 3000
+
+const app = express()
+app.use(cors({ origin: true }))
+
+var serviceAccount = require('./permissions.json')
+const { response } = require('express')
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
+
+const db = admin.firestore()
+
+app.get('/:collection/:document/:subject/:grade', (req, res) => {
+    const database = db.collection(req.params.collection).doc(req.params.document)
+    const subject = req.params.subject
+    const grade = req.params.grade
+    let obj = {}
+    obj[subject] = grade
+    database.set(obj)
+    return res.status(200).send(response)
+})
+
+app.listen(port, () => {console.log("listening on port" + port)})
