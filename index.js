a427d3cd52f071ca26c1b2b670cb123a54e09687
ngeor26 <ngeor26@germantownacademy.org>
2023-03-12 15:25:55 -0400

    added headers and cors back idk why

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-const admin = require('firebase-admin')
-const express = require('express')
-const port = process.env.PORT || 3000
+const admin = require("firebase-admin");
+const express = require("express");
+const port = process.env.PORT || 3000;
+const cors = require("cors");
 
-const app = express()
+const app = express();
+app.use(cors());
 
-var serviceAccount = require('./permissions.json')
-const { response } = require('express')
+var serviceAccount = require("./permissions.json");
+const { response } = require("express");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-})
+  credential: admin.credential.cert(serviceAccount),
+});
 
-const db = admin.firestore()
+const db = admin.firestore();
 
-app.get('/:collection/:document/:subject/:grade', (req, res) => {
-    const database = db.collection(req.params.collection).doc(req.params.document)
-    const subject = req.params.subject
-    const grade = req.params.grade
-    let obj = {}
-    obj[subject] = grade
-    database.set(obj, { merge: true })
-    return res.status(200).send(response)
-})
+app.get("/:collection/:document/:subject/:grade", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  const database = db
+    .collection(req.params.collection)
+    .doc(req.params.document);
+  const subject = req.params.subject;
+  const grade = req.params.grade;
+  let obj = {};
+  obj[subject] = grade;
+  database.set(obj, { merge: true });
+  return res.status(200).send(response);
+});
 
-app.listen(port, () => {console.log("listening on port" + port)})
+app.listen(port, () => {
+  console.log("listening on port" + port);
+});
